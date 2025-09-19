cdd6e54362eaa971703d726b6b5d8037c8a00522
Pravin Shinde <pravins@xanthus.in>
2025-09-19 16:47:22 +0530

    test tool

--- user_db.py
+++ user_db.py
@@ -6,6 +6,7 @@ def check_user_credentials(username, password):
     
     # VULNERABILITY: The username is inserted directly into the query string.
     # An attacker could use the username "' OR '1'='1" to log in as any user.
+    # testing 
     query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
     cursor.execute(query)
     
