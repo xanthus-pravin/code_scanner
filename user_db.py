175d87b75403085af4c812d52d54f0ad27141109
Pravin Shinde <pravins@xanthus.in>
2025-09-19 16:57:01 +0530

    testing

---
+++ user_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def check_user_credentials(username, password):
+    db = sqlite3.connect("users.db")
+    cursor = db.cursor()
+    
+    # VULNERABILITY: The username is inserted directly into the query string.
+    # An attacker could use the username "' OR '1'='1" to log in as any user.
+    # testing 
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    cursor.execute(query)
+    
+    result = cursor.fetchone()
+    return result is not None
